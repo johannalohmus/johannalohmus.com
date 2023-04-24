3dc177ffc7621310f6f9788a8997d9842964acf2
Johanna Lohmus <johanna.lohmus@gmail.com>
2023-04-24 11:10:39 -0400

    first commit

---
+++ src/script.js
@@ -0,0 +1,48 @@
+import * as THREE from 'three'
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector(".webgl"),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+renderer.render(scene, camera);
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshBasicMaterial({color: 0xFF6347, wireframe: true});
+const torus = new THREE.Mesh(geometry, material);
+scene.add(torus);
+
+function animate()
+{
+  requestAnimationFrame(animate);
+  torus.rotation.x += 0.1;
+  torus.rotation.y += 0.1;
+  torus.rotation.z += 0.1;
+  renderer.render(scene, camera);
+}
+
+animate();
+
+function reveal() {
+  var reveals = document.querySelectorAll("section");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+reveal();
