40ead46bacb34481a88ea999e1884501c92cf1ed
Johanna Lohmus <johanna.lohmus@gmail.com>
2023-05-14 22:32:08 -0400

    moon 4

--- src/script.js
+++ src/script.js
@@ -45,7 +45,7 @@ function addStar()
   scene.add(star);
 }
 
-const moonTexture = new THREE.TextureLoader().load('../moon.jpg');
+const moonTexture = new THREE.TextureLoader().load('moon.jpg');
 const normalTexture = new THREE.TextureLoader().load('../normal.jpg');
 
 const moon = new THREE.Mesh(
