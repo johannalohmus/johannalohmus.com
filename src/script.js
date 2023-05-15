d2226642cc0f17a166fd2a9c8952737db2dc1fd1
Johanna Lohmus <johanna.lohmus@gmail.com>
2023-05-14 22:11:35 -0400

    lights and orbit

--- src/script.js
+++ src/script.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three'
 
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
@@ -19,12 +21,28 @@ const material = new THREE.MeshBasicMaterial({color: 0xFF6347, wireframe: true})
 const torus = new THREE.Mesh(geometry, material);
 scene.add(torus);
 
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5, 5, 5);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
+
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+
 function animate()
 {
   requestAnimationFrame(animate);
   torus.rotation.x += 0.2;
   torus.rotation.y += 0.2;
   torus.rotation.z += 0.1;
+
+  controls.update();
+
   renderer.render(scene, camera);
 }
 
