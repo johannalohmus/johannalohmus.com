03b8876d4aceabd693758e2aca279b1fbd37ee2d
Johanna Lohmus <johanna.lohmus@gmail.com>
2023-05-14 22:45:45 -0400

    moon path

--- src/script.js
+++ src/script.js
@@ -45,7 +45,7 @@ function addStar()
   scene.add(star);
 }
 
-const moonTexture = new THREE.TextureLoader().load('./static/images/earth.jpg');
+const moonTexture = new THREE.TextureLoader().load('./public/static/images/earth.jpg');
 const normalTexture = new THREE.TextureLoader().load('../normal.jpg');
 
 const moon = new THREE.Mesh(
